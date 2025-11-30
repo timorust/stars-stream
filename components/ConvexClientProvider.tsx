336c3c2d45a4d7161c10c38c279f4e7e3f6e04d1
Tamir Rostamov <tamir@el-ish.local>
2025-11-30 11:36:48 +0200

    start stars-stream app

---
+++ components/ConvexClientProvider.tsx
@@ -0,0 +1,21 @@
+"use client";
+import { useAuth } from "@clerk/nextjs";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import { ConvexReactClient } from "convex/react";
+
+const convexUrl = process.env.NEXT_PUBLIC_CONVEX_URL;
+
+if (!convexUrl)
+  throw new Error("Missing NEXT_PUBLIC_CONVEX_URL environment variable");
+
+const convex = new ConvexReactClient(convexUrl);
+
+function ConvexClientProvider({ children }: { children: React.ReactNode }) {
+  return (
+    <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+      {children}
+    </ConvexProviderWithClerk>
+  );
+}
+
+export default ConvexClientProvider;
