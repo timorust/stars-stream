77e117186772f639694de061b03e95edd511c1b7
Tamir Rustamov <timorust1@gmail.com>
2025-12-01 07:50:11 +0200

    clerk-signin

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Header from "@/components/Header";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="">
+      <Header />
       <h1>Hello TIMIK</h1>
     </div>
   );
---
+++ components/Header.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { SignInButton, UserButton } from "@clerk/nextjs";
+import { Authenticated, Unauthenticated } from "convex/react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+import { Button } from "./ui/button";
+
+const Header = () => {
+  const pathName = usePathname();
+  const isDashboard = pathName.startsWith("/dashboard");
+
+  return (
+    <header className="flex items-center justify-between px-4 h-15 sm:px-6">
+      <Link href="/dashboard" className="font-medium uppercase tracking-widest">
+        Stream
+      </Link>
+
+      <div className="flex items-center gap-2">
+        <Authenticated>
+          {!isDashboard && (
+            <Link href="/dashboard">
+              <Button variant="outline">Dashboard</Button>
+            </Link>
+          )}
+          <UserButton />
+        </Authenticated>
+
+        <Unauthenticated>
+          <SignInButton
+            mode="modal"
+            forceRedirectUrl="/dashboard"
+            signUpForceRedirectUrl="/dashboard"
+          >
+            <Button variant="outline">Sign In</Button>
+          </SignInButton>
+        </Unauthenticated>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
