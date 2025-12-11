f0b3fcc357038929491024da9677f6c84f1a9f49
Tamir Rustamov <timorust1@gmail.com>
2025-12-11 10:32:48 +0200

    stream-chat-react

--- app/(signed-in)/layout.tsx
+++ app/(signed-in)/layout.tsx
@@ -1,9 +1,15 @@
 "use client";
 
+import { Chat } from "stream-chat-react";
 import UserSyncWrapper from "@/components/UserSyncWrapper";
+import streamClient from "@/lib/stream";
 
 function Layout({ children }: { children: React.ReactNode }) {
-  return <UserSyncWrapper>{children}</UserSyncWrapper>;
+  return (
+    <UserSyncWrapper>
+      <Chat client={streamClient}>{children}</Chat>
+    </UserSyncWrapper>
+  );
 }
 
 export default Layout;
