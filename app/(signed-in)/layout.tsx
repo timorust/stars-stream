694d7486ab8feb06d750c1a04db16093f1781c24
Tamir Rustamov <timorust1@gmail.com>
2025-12-12 07:16:50 +0200

    Sidebar

--- app/(signed-in)/layout.tsx
+++ app/(signed-in)/layout.tsx
@@ -1,13 +1,54 @@
 "use client";
+import React from "react";
 
 import { Chat } from "stream-chat-react";
+import { Separator } from "@/components/ui/separator";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
 import UserSyncWrapper from "@/components/UserSyncWrapper";
 import streamClient from "@/lib/stream";
+import { AppSidebar } from "@/components/app-sidebar";
+import Link from "next/link";
 
 function Layout({ children }: { children: React.ReactNode }) {
   return (
     <UserSyncWrapper>
-      <Chat client={streamClient}>{children}</Chat>
+      <Chat client={streamClient}>
+        <SidebarProvider
+          style={
+            {
+              "--sidebar-width": "19rem",
+            } as React.CSSProperties
+          }
+        >
+          <AppSidebar />
+          <SidebarInset>
+            <header className="flex h-16 shrink-0 items-center gap-2 px-4">
+              <SidebarTrigger className="-ml-1" />
+              <Separator
+                orientation="vertical"
+                className="mr-2 data-[orientation=vertical]:h-4"
+              />
+              <Link href="/dashboard">
+                <h1 className="text-lg font-bold -tracking-wider uppercase">
+                  Dream Stream
+                </h1>
+              </Link>
+            </header>
+            <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+              <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+                <div className="bg-muted/50 aspect-video rounded-xl" />
+                <div className="bg-muted/50 aspect-video rounded-xl" />
+                <div className="bg-muted/50 aspect-video rounded-xl" />
+              </div>
+              <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" />
+            </div>
+          </SidebarInset>
+        </SidebarProvider>
+      </Chat>
     </UserSyncWrapper>
   );
 }
